48b5ae0cd68671b422ccd172a54cf33e2a2632cd
mnath6 <mirna600@gmail.com>
2017-06-22 15:28:46 -0700

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,8 @@
+$('.nav-trigger').click(function() {
+  $('.main-nav').addClass('showit');
+  $('.nav-close').addClass('showit');
+});
+$('.nav-close').click(function() {
+  $('.main-nav').removeClass('showit');
+  $('.nav-close').removeClass('showit');
+});s
