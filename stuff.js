1ea0704182310234c497c01c3ff60cea235689f0
mnath6 <mirna600@gmail.com>
2017-06-26 11:41:13 -0700

    Delete stuff.js

--- stuff.js
+++
@@ -1,8 +0,0 @@
-$('.nav-trigger').click(function() {
-  $('.main-nav').addClass('showit');
-  $('.nav-close').addClass('showit');
-});
-$('.nav-close').click(function() {
-  $('.main-nav').removeClass('showit');
-  $('.nav-close').removeClass('showit');
-});s
